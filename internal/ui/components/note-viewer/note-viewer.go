d162b022c370ed3edf28cfe96967f00179cdfa1e
Ben Webb <ben.webb340@gmail.com>
2024-11-18 10:52:40 +0900

    list and viewport components

---
+++ internal/ui/components/note-viewer/note-viewer.go
@@ -0,0 +1,127 @@
+package noteviewer
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/charmbracelet/bubbles/viewport"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+// You generally won't need this unless you're processing stuff with
+// complicated ANSI escape sequences. Turn it on if you notice flickering.
+//
+// Also keep in mind that high performance rendering only works for programs
+// that use the full size of the terminal. We're enabling that below with
+// tea.EnterAltScreen().
+const useHighPerformanceRenderer = false
+
+var (
+	titleStyle = func() lipgloss.Style {
+		b := lipgloss.RoundedBorder()
+		b.Right = "├"
+		return lipgloss.NewStyle().BorderStyle(b).Padding(0, 1)
+	}()
+
+	infoStyle = func() lipgloss.Style {
+		b := lipgloss.RoundedBorder()
+		b.Left = "┤"
+		return titleStyle.BorderStyle(b)
+	}()
+)
+
+type NoteViewerModel struct {
+	textContent string
+	title       string
+	ready       bool
+	viewport    viewport.Model
+}
+
+func New(title, textContent string) *NoteViewerModel {
+	m := new(NoteViewerModel)
+	m.title = title
+	m.textContent = textContent
+	return m
+}
+
+func (m *NoteViewerModel) SetNoteContent(title string, text string) {
+	m.title = title
+	m.textContent = text
+}
+
+func (m NoteViewerModel) Init() tea.Cmd {
+	return nil
+}
+
+func (m NoteViewerModel) Update(msg tea.Msg) (NoteViewerModel, tea.Cmd) {
+	var cmd tea.Cmd
+	var cmds []tea.Cmd
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		if k := msg.String(); k == "ctrl+c" || k == "q" || k == "esc" {
+			return m, tea.Quit
+		}
+	case tea.WindowSizeMsg:
+		headerHeight := lipgloss.Height(m.headerView())
+		footerHeight := lipgloss.Height(m.footerView())
+		verticalMarginHeight := headerHeight + footerHeight
+
+		if !m.ready {
+			// Since this program is using the full size of the viewport we
+			// need to wait until we've received the window dimensions before
+			// we can initialize the viewport. The initial dimensions come in
+			// quickly, though asynchronously, which is why we wait for them
+			// here.
+			m.viewport = viewport.New(msg.Width, msg.Height-verticalMarginHeight)
+			m.viewport.YPosition = headerHeight
+			m.viewport.HighPerformanceRendering = useHighPerformanceRenderer
+			m.viewport.SetContent(m.textContent)
+			m.ready = true
+
+			// This is only necessary for high performance rendering, which in
+			// most cases you won't need.
+			//
+			// Render the viewport one line below the header.
+			m.viewport.YPosition = headerHeight + 1
+		} else {
+			m.viewport.Width = msg.Width
+			m.viewport.Height = msg.Height - verticalMarginHeight
+		}
+
+		if useHighPerformanceRenderer {
+			// Render (or re-render) the whole viewport. Necessary both to
+			// initialize the viewport and when the window is resized.
+			//
+			// This is needed for high-performance rendering only.
+			cmds = append(cmds, viewport.Sync(m.viewport))
+		}
+	}
+
+	m.viewport, cmd = m.viewport.Update(msg)
+	cmds = append(cmds, cmd)
+
+	return m, tea.Batch(cmds...)
+}
+
+func (m NoteViewerModel) View() string {
+	if !m.ready {
+		return "\n Initializing..."
+	}
+	return fmt.Sprintf("%s\n%s\n%s", m.headerView(), m.viewport.View(), m.footerView())
+}
+
+func (m NoteViewerModel) headerView() string {
+	title := titleStyle.Render(fmt.Sprintf("Notes: %s", m.title))
+	line := strings.Repeat("─", max(0, m.viewport.Width-lipgloss.Width(title)))
+	return lipgloss.JoinHorizontal(lipgloss.Center, title, line)
+}
+
+func (m NoteViewerModel) footerView() string {
+	info := infoStyle.Render(fmt.Sprintf("%3.f%%", m.viewport.ScrollPercent()*100))
+	line := strings.Repeat("─", max(0, m.viewport.Width-lipgloss.Width(info)))
+	nav := "ESC: exit"
+	footerLine := lipgloss.JoinHorizontal(lipgloss.Center, line, info)
+	return lipgloss.JoinVertical(lipgloss.Left, footerLine, nav)
+}
--- internal/ui/task-ui/task-ui.go
+++ internal/ui/task-ui/task-ui.go
@@ -2,42 +2,18 @@ package taskui
 
 import (
 	"fmt"
-	"strings"
 
-	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
 	"github.com/webbben/task/internal/tasks"
 	"github.com/webbben/task/internal/types"
-)
-
-// You generally won't need this unless you're processing stuff with
-// complicated ANSI escape sequences. Turn it on if you notice flickering.
-//
-// Also keep in mind that high performance rendering only works for programs
-// that use the full size of the terminal. We're enabling that below with
-// tea.EnterAltScreen().
-const useHighPerformanceRenderer = false
-
-var (
-	titleStyle = func() lipgloss.Style {
-		b := lipgloss.RoundedBorder()
-		b.Right = "├"
-		return lipgloss.NewStyle().BorderStyle(b).Padding(0, 1)
-	}()
-
-	infoStyle = func() lipgloss.Style {
-		b := lipgloss.RoundedBorder()
-		b.Left = "┤"
-		return titleStyle.BorderStyle(b)
-	}()
+	listcomponent "github.com/webbben/task/internal/ui/components/list-component"
+	noteviewer "github.com/webbben/task/internal/ui/components/note-viewer"
 )
 
 type model struct {
-	content     *types.Task
-	textContent string
-	ready       bool
-	viewport    viewport.Model
+	content    *types.Task
+	noteViewer noteviewer.NoteViewerModel
+	noteList   listcomponent.ListComponentModel
 }
 
 func (m model) Init() tea.Cmd {
@@ -48,86 +24,24 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
 	var cmds []tea.Cmd
 
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		if k := msg.String(); k == "ctrl+c" || k == "q" || k == "esc" {
-			return m, tea.Quit
-		}
-	case tea.WindowSizeMsg:
-		headerHeight := lipgloss.Height(m.headerView())
-		footerHeight := lipgloss.Height(m.footerView())
-		verticalMarginHeight := headerHeight + footerHeight
-
-		if !m.ready {
-			// Since this program is using the full size of the viewport we
-			// need to wait until we've received the window dimensions before
-			// we can initialize the viewport. The initial dimensions come in
-			// quickly, though asynchronously, which is why we wait for them
-			// here.
-			m.viewport = viewport.New(msg.Width, msg.Height-verticalMarginHeight)
-			m.viewport.YPosition = headerHeight
-			m.viewport.HighPerformanceRendering = useHighPerformanceRenderer
-			m.viewport.SetContent(m.textContent)
-			m.ready = true
-
-			// This is only necessary for high performance rendering, which in
-			// most cases you won't need.
-			//
-			// Render the viewport one line below the header.
-			m.viewport.YPosition = headerHeight + 1
-		} else {
-			m.viewport.Width = msg.Width
-			m.viewport.Height = msg.Height - verticalMarginHeight
-		}
-
-		if useHighPerformanceRenderer {
-			// Render (or re-render) the whole viewport. Necessary both to
-			// initialize the viewport and when the window is resized.
-			//
-			// This is needed for high-performance rendering only.
-			cmds = append(cmds, viewport.Sync(m.viewport))
-		}
-	}
-
-	m.viewport, cmd = m.viewport.Update(msg)
+	// note viewer
+	newNoteViewerModel, cmd := m.noteViewer.Update(msg)
+	m.noteViewer = newNoteViewerModel
 	cmds = append(cmds, cmd)
 
 	return m, tea.Batch(cmds...)
 }
 
-func (m *model) setTextContent() {
-	if m.content == nil {
-		return
-	}
-	if m.content.Notes == nil {
-		m.textContent = "No notes found."
-		return
-	}
-
-	var out string
-	for date, note := range m.content.Notes {
-		out = fmt.Sprintf("%s%s\n\n%s\n\n", out, date, note)
-	}
-	m.textContent = out
-}
-
 func (m model) View() string {
-	if !m.ready {
-		return "\n Initializing..."
+	return m.noteViewer.View()
+}
+
+func (m *model) setSelectedNote(noteTitle string) {
+	noteContent, exists := m.content.Notes[noteTitle]
+	if !exists {
+		return
 	}
-	return fmt.Sprintf("%s\n%s\n%s", m.headerView(), m.viewport.View(), m.footerView())
-}
-
-func (m model) headerView() string {
-	title := titleStyle.Render(fmt.Sprintf("Notes: %s", m.content.Title))
-	line := strings.Repeat("─", max(0, m.viewport.Width-lipgloss.Width(title)))
-	return lipgloss.JoinHorizontal(lipgloss.Center, title, line)
-}
-
-func (m model) footerView() string {
-	info := infoStyle.Render(fmt.Sprintf("%3.f%%", m.viewport.ScrollPercent()*100))
-	line := strings.Repeat("─", max(0, m.viewport.Width-lipgloss.Width(info)))
-	return lipgloss.JoinHorizontal(lipgloss.Center, line, info)
+	m.noteViewer.SetNoteContent(noteTitle, noteContent)
 }
 
 func RunUI(taskID string) error {
@@ -137,9 +51,9 @@ func RunUI(taskID string) error {
 	}
 
 	m := model{
-		content: task,
+		content:    task,
+		noteViewer: *noteviewer.New("", ""),
 	}
-	m.setTextContent()
 
 	p := tea.NewProgram(m, tea.WithAltScreen(), tea.WithMouseCellMotion())
 
